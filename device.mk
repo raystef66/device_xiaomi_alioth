a2344ec2423d4995bd9519562b7cac5a9978537c
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-06-23 19:18:19 +0530

    alioth: overlay: Configure device layout specific overlays
    Co-Authored-by: Dyneteve <dyneteve@hentaios.com>
    Signed-off-by: revolwoc <imsakshisharma04@gmail.com>

--- device.mk
+++ device.mk
@@ -31,6 +31,10 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+PRODUCT_PACKAGES += \
+    AliothFrameworks \
+    AliothSystemUI
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 30
 
