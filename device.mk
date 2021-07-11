e658726d80c9807630cd19b21a7a984ea187c9ae
maytinhdibo <iammaytinhdibo@gmail.com>
2021-07-11 17:50:01 +0700

    alioth: Add new pocket mode
    -: By @maytinhdibo
    -: update event for fingerprint wake up screen
    -: remove duplicate event turn on service
    -: Made it more battery friendly
    -: Reworked pocket utils

--- device.mk
+++ device.mk
@@ -42,6 +42,13 @@ PRODUCT_PACKAGES += \
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
+# Pocket Mode
+PRODUCT_PACKAGES += \
+    PocketMode
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/pocket/privapp-permissions-pocketmode.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-pocketmode.xml
+
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
