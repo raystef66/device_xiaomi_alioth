0312878685ab5ca668c858df3140d6b29bd6f0f9
SahilSonar <sss.sonar2003@gmail.com>
2021-07-30 06:18:54 +0200

    alioth: overlay-lineage: Set proper fingerprint sensor location
    Change-Id: I5870100b1df2004f0541bba22f7a355234d082c6

--- device.mk
+++ device.mk
@@ -15,7 +15,8 @@ $(call inherit-product, device/xiaomi/sm8250-common/kona.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Pocket Mode
 PRODUCT_PACKAGES += \
