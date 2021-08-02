fc01ee6c07320b2a16fd1aa6d7748df5d8d63001
GtrCraft <nickvanbokhorst@hotmail.com>
2021-08-02 19:39:43 +0200

    alioth: Use redfin fingerprint
    * To pass safetynet
    
    Change-Id: Ic73b2adeceaa59429845d420ce3f40f3e063919b

--- libinit/init_alioth.cpp
+++ libinit/init_alioth.cpp
@@ -6,6 +6,9 @@
 
 #include <libinit_kona.h>
 
+#define DESCRIPTION "redfin-user 11 RQ3A.210705.001 7380771 release-keys"
+#define FINGERPRINT "google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys"
+
 static const variant_info_t aliothcn_info = {
     .hwc_value = "CN",
     .sku_value = "",
@@ -14,8 +17,8 @@ static const variant_info_t aliothcn_info = {
     .device = "alioth",
     .marketname = "K40",
     .model = "M2012K11AC",
-    .build_description = "alioth-user 11 RKQ1.200826.002 V12.5.4.0.RKHCNXM release-keys",
-    .build_fingerprint = "Redmi/alioth/alioth:11/RKQ1.200826.002/V12.5.4.0.RKHCNXM:user/release-keys",
+    .build_description = DESCRIPTION,
+    .build_fingerprint = FINGERPRINT,
 
     .nfc = true,
 };
@@ -28,8 +31,8 @@ static const variant_info_t aliothin_info = {
     .device = "aliothin",
     .marketname = "Mi 11X",
     .model = "M2012K11AI",
-    .build_description = "aliothin-user 11 RKQ1.200826.002 V12.5.2.0.RKHMIXM release-keys",
-    .build_fingerprint = "Mi/aliothin/aliothin:11/RKQ1.200826.002/V12.5.2.0.RKHMIXM:user/release-keys",
+    .build_description = DESCRIPTION,
+    .build_fingerprint = FINGERPRINT,
 
     .nfc = false,
 };
@@ -42,8 +45,8 @@ static const variant_info_t alioth_info = {
     .device = "alioth",
     .marketname = "POCO F3",
     .model = "M2012K11AG",
-    .build_description ="alioth-user 11 RKQ1.200826.002 V12.5.2.0.RKHMIXM release-keys",
-    .build_fingerprint = "POCO/alioth_global/alioth:11/RKQ1.200826.002/V12.5.2.0.RKHMIXM:user/release-keys",
+    .build_description = DESCRIPTION,
+    .build_fingerprint = FINGERPRINT,
 
     .nfc = true,
 };
