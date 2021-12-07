e4459cf317c2928eb9da929943131399d3550e35
SahilSonar <sss.sonar2003@gmail.com>
2021-12-07 11:39:00 +0000

    alioth: Update build fingerprint from redfin SQ1A.211205.008

--- libinit/init_xiaomi_alioth.cpp
+++ libinit/init_xiaomi_alioth.cpp
@@ -10,7 +10,7 @@
 #include "vendor_init.h"
 
 #define DESCRIPTION "alioth_global-user 11 RKQ1.200826.002 V12.5.3.0.RKHMIXM release-keys"
-#define FINGERPRINT "google/redfin/redfin:12/SP1A.210812.015/7679548:user/release-keys"
+#define FINGERPRINT "google/redfin/redfin:12/SQ1A.211205.008/7888514:user/release-keys"
 
 static const variant_info_t aliothcn_info = {
     .hwc_value = "CN",
