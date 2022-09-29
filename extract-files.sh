07117953f9dc0d5a37aec1afaf62208f55965e8d
johnmart19 <johnivan19999@gmail.com>
2022-09-29 22:36:52 +0300

    Alioth: Use Proper NFC Fixup - With value 1 NFC is unable to read NFC Tags and Transport Cards

--- extract-files.sh
+++ extract-files.sh
@@ -13,7 +13,7 @@ function blob_fixup() {
 # Mifare Tag implementation
 # 0: General implementation
 # 1: Legacy implementation
-LEGACY_MIFARE_READER=1
+LEGACY_MIFARE_READER=0
 EOF
             ;;
         vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
